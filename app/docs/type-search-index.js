7ebdb0039d64ef0bd9fce4dc2155e4900722f43c
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2022-03-19 17:41:59 -0500

    fixed javadoc output

---
+++ app/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Blackjack","l":"App"},{"p":"Blackjack","l":"Card"},{"p":"Blackjack","l":"Player"},{"p":"Blackjack","l":"Table"}]
