c851f924b189d895f40f27b3458c20b385a0b56c
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2022-03-18 19:38:46 -0500

    updated makefile again, removed storing docs in remote

---
+++ Makefile
@@ -0,0 +1,43 @@
+# A simple makefile for compiling three java classes
+#
+
+# define a makefile variable for the java compiler
+
+JCC = javac
+
+# define a makefile variable for compilation flags
+# the -g flag compiles with debugging information
+
+JFLAGS = -g -d
+
+rootdir = .
+
+CLASSPATH = $(rootdir)/src:$(rootdir)/tests
+
+sourcelist = $(shell find $(rootdir) -name '*.java' | sed "s,[.]/,,g")
+
+docdir = ./docs
+
+classdir = ./classes
+
+# typing 'make' will invoke the first target entry in the makefile 
+# (the default one in this case)
+
+CLASSES = Table.java Card.java Player.java
+
+default: all
+
+all:
+	@javac -cp $(CLASSPATH) -d ./classes $(sourcelist)
+	@javadoc -d $(docdir) -linksource $(sourcelist)
+
+# To start over from scratch, type 'make clean'.  
+# Removes all .class files, so that the next make rebuilds them
+clean:
+	# Remove all of the documentation
+	@if [ -d $(docdir) ]; then rm -r $(docdir); fi;
+	# Remove all the class files in the classpath
+	@-find $(rootdir) \( -name "*~" -o -name "*.class" -o -name "runTests" \) -exec rm '{}' \;
+	# Remove Classes directory
+	@if [ -d $(classdir) ]; then rm -r $(classdir)/*; fi;
+
--- game/Player.java
+++ game/Player.java
@@ -2,34 +2,71 @@ package game;
 
 import java.util.ArrayList;
 
+/**
+ * This class represents a player playing at the table. A Player has a name and a balance that they can use to bet
+ * during the game.
+ *
+ * @author Brian Wang and Jonathan Leibovich
+ * @version 1.0
+ * @since 2022-03-18
+ */
 public class Player {
+
     String name;
+
     int score;
+
     ArrayList<Card> cards;
-    
+
+    /**
+     * Returns the player's name
+     *
+     * @param name player's name
+     */
     public Player(String name){
         this.name = name;
         this.score = 0;
         cards = new ArrayList<>();
     }
 
+    /**
+     * Returns the value of the player's name
+     *
+     * @return Value of current hand
+     */
     public int getScore(){
         return this.score;
     }
 
+    /**
+     * Returns the player's name
+     *
+     * @return Player's name
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Adds card to player's hand.
+     *
+     * @param card Add card to hand for the round
+     */
     public void addCard(Card card){
         cards.add(card);
         this.score += card.getNum();
     }
 
+    /**
+     * Remove the cards from your hand
+     */
     public void noCards(){
         cards.clear();
     }
 
+    /**
+     * Display the cards in your hands as well as the cards combined value
+     */
     public void displayCards(){
         System.out.println(name + " has:\n");
         for (int i = 0; i < this.cards.size(); i++){
